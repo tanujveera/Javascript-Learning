da63a8abd81cf189bc96c38ccacbad0bb4021d63
tanujveera <tanuj.veera@gmail.com>
2023-10-02 15:06:02 +0530

    CallBack hell and Inversion of control

---
+++ Callback Hell.js
@@ -0,0 +1,26 @@
+//Call back hell
+//Call back function:
+//A function is passed as an argument into another function is call back function.
+setTimeout(function y(){
+  console.log("Call back function")
+},5000);
+
+//Consider there is a website you are purchasing
+//There are few tasks performed like order creation, payment, order summary etc. all are dependent on one another.
+
+const cart = ["shirt", "pant", "shoes"];
+//Here api is just using the external API and perform few operations
+api.createOrder(cart, function (){
+
+  api.paymentOrder(function (){
+
+    api.orderSummary();
+  
+  })
+
+});
+
+//The process continues and many call backs are called and there is no clarity as to what is happening.
+
+//Inversion of control
+//Since the api is external and we don't know whether it will work or not. Control is not in developer's hands (our hands). Thus , the name Inversion of Control.
