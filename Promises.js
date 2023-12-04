7b298b2e7a70578844400459825f77130187db84
tanujveera <68957796+tanujveera@users.noreply.github.com>
2023-12-04 07:45:45 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -1,6 +1,9 @@
 //Promises
 /*The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
-A Promise is a proxy for a value not necessarily known when the promise is created. It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, the asynchronous method returns a promise to supply the value at some point in the future.
+A Promise is a proxy for a value not necessarily known when the promise is created. 
+It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. 
+This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, 
+the asynchronous method returns a promise to supply the value at some point in the future.
 
 In other words, it is a placeholder/ container for a future value when promise is fulfilled
 */
